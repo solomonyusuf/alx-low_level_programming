a0511780de94912d2cdd8fc3764c4556c02b473a
Solomon yusuf <81124240+solomonyusuf@users.noreply.github.com>
2022-10-15 03:37:11 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Prints all combinations of two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	int digit1, digit2;
+
+	
+	putchar('\n');
+	return (0);
+}
