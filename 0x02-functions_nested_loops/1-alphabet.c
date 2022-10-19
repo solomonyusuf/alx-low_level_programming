d948c910a08d466c3944329cc46208c7b1ff974b
Solomon yusuf <81124240+solomonyusuf@users.noreply.github.com>
2022-10-19 02:45:51 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+   
+  char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		print_alphabet(letter);
+		letter++;
+	}
+    return (0);
+}
