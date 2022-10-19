207e2e621e98ff87526ad998d314932f4b82544e
Solomon yusuf <81124240+solomonyusuf@users.noreply.github.com>
2022-10-19 03:21:05 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+es (19 sloc) 274 Bytes
+#include <stdio.h>
+
+/**
+ * main: multiples of 3 or 5 up to 102
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
