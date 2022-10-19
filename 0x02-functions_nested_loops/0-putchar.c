dc3e09064d99ed744f35c39562b9bd9737770fb1
Solomon yusuf <81124240+solomonyusuf@users.noreply.github.com>
2022-10-19 02:30:05 +0100

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description: prints 'Holberton \n'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+string str = "Holberton\n";
+int i = 0;
+_putchar(str);
+return (0);
+}
